99c625d41cf4842b538d60416f9fd7a02b24e064
George McIntosh <george@elevenware.com>
2015-11-11 15:29:21 +0000

    adding runner

---
+++ jenkins.groovy
@@ -0,0 +1,9 @@
+@GrabResolver(name='oss', root='https://oss.sonatype.org/content/repositories/snapshots/')
+@Grab('com.elevenware:the-baker:0.1-SNAPSHOT')
+@GrabExclude('org.jenkins-ci.main:jenkins-core')
+@GrabExclude('org.jenkins-ci.plugins:job-dsl-core')
+
+import static com.elevenware.baker.JenkinsEnvironment.configure
+
+configure("./estate.json")
+
