63a132cb3e4587099f40e21513812aea4ed0e661
George McIntosh <george@elevenware.com>
2015-11-11 15:42:05 +0000

    rfresh dependencies

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name='oss', root='https://oss.sonatype.org/content/repositories/snapshots/')
-@Grab('com.elevenware:the-baker:0.1-SNAPSHOT')
+@Grab('com.elevenware:the-baker:0.1-SNAPSHOT', changing=true)
 @GrabExclude('org.jenkins-ci.main:jenkins-core')
 @GrabExclude('org.jenkins-ci.plugins:job-dsl-core')
 
