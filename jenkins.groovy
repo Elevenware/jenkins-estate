2e07328f11f91b15510cebcb526a0fa89f4ef376
George McIntosh <george@elevenware.com>
2015-11-11 15:45:39 +0000

    expand grab

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name='oss', root='https://oss.sonatype.org/content/repositories/snapshots/')
-@Grab('com.elevenware:the-baker:0.1-SNAPSHOT', changing=true)
+@Grab(group='com.elevenware', module='the-baker', version='0.1-SNAPSHOT', changing=true)
 @GrabExclude('org.jenkins-ci.main:jenkins-core')
 @GrabExclude('org.jenkins-ci.plugins:job-dsl-core')
 
